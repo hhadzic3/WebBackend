bb30147ebca87b9c43b31ef0669b7d0c2f813821
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-05-02 17:39:53 +0200

    uspjesno testirana metoda DELETE

--- test/test.js
+++ test/test.js
@@ -6,8 +6,27 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('Testiranje servera', function() {
+    let part = {id:69,name: 'Brisaci'} 
+    let review = { id:69,state:"zavrsen", kind: "redovni", responsible_person:2, vehicle:2 }
+    let failure = { id:69, name: "mjenjac", vehicle: 2 } 
+    let user = {
+        id:69,
+        first_name: "Miki",
+        last_name: "Mustafic",
+        position: "administrator",
+        jmbg: "88855582313",
+        birth_date: "1.1.2002",
+        adress: "Bakije 12",
+        zip_code: "71000",
+        mail: "Mikica@gmail.com",
+        phone_number: "061-333-221",
+        user_name: "muki",
+        password: "miki"
+    }
+    let vehicle = { owner_name: "John",brand: "BMV",type: "putnicko",
+            serial_number: "999412",production_year: 2020,date_of_use: '1.1.2020',previous_inspection: '3.3.2020'};
     
-    describe('GET', ()=> {
+            describe('GET', ()=> {
         it('GET /api/user', (done) => {
             chai.request(app)
             .get('/api/user')
@@ -35,6 +54,7 @@ describe('Testiranje servera', function() {
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
+                res.should.be.json;
                 res.body[0].should.have.property('kind');
                 done();
             }); 
@@ -44,6 +64,7 @@ describe('Testiranje servera', function() {
             .get('/api/part')
             .end((err, res) => {
                 res.should.have.status(200);
+                res.should.be.json;
                 res.body.should.be.a('array');
                 res.body[0].should.have.property('name');
                 done();
@@ -54,6 +75,7 @@ describe('Testiranje servera', function() {
             .get('/api/vehicle')
             .end((err, res) => {
                 res.should.have.status(200);
+                res.should.be.json;
                 res.body.should.be.a('array');
                 res.body[0].should.have.property('type');
                 done();
@@ -63,19 +85,6 @@ describe('Testiranje servera', function() {
 
     describe('POST', ()=> {
         it('POST /api/user', (done) => {
-            let user = {
-                first_name: "Miki",
-                last_name: "Mustafic",
-                position: "administrator",
-                jmbg: "88855582313",
-                birth_date: "1.1.2002",
-                adress: "Bakije 12",
-                zip_code: "71000",
-                mail: "Mikica@gmail.com",
-                phone_number: "061-333-221",
-                user_name: "muki",
-                password: "miki"
-            }    
             chai.request(app)
             .post('/api/user')
             .send(user)
@@ -88,7 +97,6 @@ describe('Testiranje servera', function() {
             }); 
         });
         it('POST /api/failure', (done) => {
-            let failure = { name: "mjenjac", vehicle: 2 } 
             chai.request(app)
             .post('/api/failure')
             .send(failure)
@@ -100,7 +108,7 @@ describe('Testiranje servera', function() {
             }); 
         });
         it('POST /api/part', (done) => {
-            let part = {name: 'Brisaci'}  
+             
             chai.request(app)
             .post('/api/part')
             .send(part)
@@ -112,7 +120,6 @@ describe('Testiranje servera', function() {
             }); 
         });
         it('POST /api/review', (done) => {
-            let review = { state:"zavrsen", kind: "redovni", responsible_person:2, vehicle:2 }
             chai.request(app)
             .post('/api/review')
             .send(review)
@@ -125,8 +132,7 @@ describe('Testiranje servera', function() {
             }); 
         });
         it('POST /api/vehicle', (done) => {
-            let vehicle = { owner_name: "John",brand: "BMV",type: "putnicko",
-            serial_number: "999412",production_year: 2020,date_of_use: '1.1.2020',previous_inspection: '3.3.2020'};
+            
             chai.request(app)
             .post('/api/vehicle')
             .send(vehicle)
@@ -138,6 +144,62 @@ describe('Testiranje servera', function() {
                 done();
             }); 
         });
+    });
+
+    describe('/DELETE/:id ', () => {
+        
+        it('it should DELETE a user given the id', (done) => {
+            chai.request(app)
+            .delete('/api/user/' + user.id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('Deleted!');
+                done();
+            });
+        });
+        it('it should DELETE a vehicle given the id', (done) => {
+            chai.request(app)
+            .delete('/api/vehicle/' + vehicle.id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('Deleted!');
+                done();
+            });
+        });
+        it('it should DELETE a part given the id', (done) => {
+            chai.request(app)
+            .delete('/api/part/' + part.id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('Deleted!');
+                done();
+            });
+        });
+        it('it should DELETE a failure given the id', (done) => {
+            chai.request(app)
+            .delete('/api/failure/' + failure.id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('Deleted!');
+                done();
+            });
+        });
+        it('it should DELETE a review given the id', (done) => {
+            chai.request(app)
+            .delete('/api/review/' + review.id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('Deleted!');
+                done();
+            });
+        });
+
+
 
     });
 
